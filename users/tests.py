42673c3f09c0f84fa098ef0e5cef5d31dc1dc71a
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2023-06-09 23:32:55 +0500

    Update User Test

--- users/tests.py
+++ users/tests.py
@@ -8,6 +8,7 @@ from django.contrib.auth import get_user
 
 
 class SignupTestCase(TestCase):
+    ### signup test | ma'lumot kiritish
     def test_signup_view(self):
         response = self.client.post(
             reverse("users:signup"),
@@ -19,22 +20,23 @@ class SignupTestCase(TestCase):
                 "password2": "Pass!123",
             }
         )
-        
+         ## kiritilgan ma'lumotni olib tekshirib korish
         user = CustomUser.objects.get(username = "defonic")
         self.assertEqual(user.first_name, "Jaloliddin")
         self.assertEqual(user.email, "123@admin.com")
         self.assertTrue(user.check_password("Pass!123"))
         
         
-        #
+        ### profil ( get) funksiyasini tekshirish
         second_response = self.client.get("/users/profile/defonic/")
         
         self.assertEqual(second_response.status_code, 200)
         
         
-        ### login
+        ### tizimga login
         self.client.login(username = "defonic", password = "Pass!123")
         
+        ## profil ma'lumotlarini yangilash funksiyani tekshirish
         third_response = self.client.post(
             reverse("users:update"),
             data = {
@@ -49,9 +51,9 @@ class SignupTestCase(TestCase):
             }
         )
         
+        ## yangilangan userni ma'lumotlarini olish
         user = get_user(self.client)
-        print(user.is_authenticated)
-        
+        # yangilangan userning datalarining yangilanganligini tekshirish
         self.assertEqual(third_response.status_code, 302)
         self.assertEqual(user.first_name, "admin")
         self.assertNotEqual(user.first_name, "Jaloliddin")
