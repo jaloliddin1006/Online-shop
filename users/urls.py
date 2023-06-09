fba6f3accee887822b10434bc9ca226b3def0646
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2023-06-09 22:23:01 +0500

    Create Profile View

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
-from .views import SignupView
+from .views import SignupView, ProfileView
 from django.urls import path
 
 app_name = "users"
 urlpatterns = [
-    path("signup", SignupView.as_view(), name="signup")
+    path("signup", SignupView.as_view(), name="signup"),
+    path("profile/<str:username>/", ProfileView.as_view(), name="profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from users.forms import SignupForm
 from django.views import View
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
 # Create your views here.
 
 
@@ -17,3 +19,9 @@ class SignupView(View):
             return redirect('login')
         return render(request, "registration/signup.html", {"form":form})
             
+            
+            
+class ProfileView(View):
+    def get(self, request, username):
+        user = get_object_or_404(CustomUser, username=username)
+        return render(request, 'profile.html', {"customuser":user})
