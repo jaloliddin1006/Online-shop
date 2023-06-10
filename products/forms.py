a928bb8655ca7dc40f8304f6c2f2a3042fd4bfcb
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2023-06-10 18:17:59 +0500

    Create post new product

--- products/forms.py
+++ products/forms.py
@@ -6,3 +6,9 @@ class NewProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ("category", "title", "description", "price", "address", "phone_number", "tg_username")
+
+    def save(self, request, commit=True):
+        product = self.instance
+        product.author = request.user
+        super().save(commit)
+        return product
--- products/views.py
+++ products/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import NewProductForm
+from .models import ProductImage
+from django.contrib import messages
 # Create your views here.
 
 
 def new_product(request):
-    form = NewProductForm
-    return render(request, "product_new.html", {"form":form})
+    if request.method == "GET":
+        form = NewProductForm()
+        return render(request, "product_new.html", {"form":form})
+    if request.method == "POST":
+        form = NewProductForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            product = form.save(request)
+            for image in request.FILES.getlist("images"):
+                ProductImage.objects.create(image=image, product=product)
+            messages.success(request,"Yangi maxsulot yaratildi")
+            return redirect("main:index")
+        return render(request, "product_new.html", {"form":form})
+        
