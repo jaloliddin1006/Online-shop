c2fe2d986576d7171e164b72ad8acbbccff0a8aa
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2023-06-10 19:19:07 +0500

    Create product_detail page

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 app_name = "products"
 urlpatterns = [
     path("new", new_product, name="new"),
-    path("<int:product_id>/detail", new_product, name="detail"),
+    path("<int:product_id>/detail", product_detail, name="detail"),
    
 ]
